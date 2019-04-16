d109a15c4cd695584a16d8c67eac4d4745b0a00d
chrispetito <christopherpetito@gmail.com>
2019-04-16 14:26:02 -0700

    add correct frontend url to sign up btns

--- js/index.js
+++ js/index.js
@@ -2,10 +2,7 @@ const homeLink = document.querySelector('.home-selected');
 homeLink.addEventListener('click', (event) => {
     event.preventDefault();
 })
-const links = document.querySelectorAll('a');
-for (var i=0; i < links.length; i++) {
-   linksin[i].addEventListener('click', (event) => {
-       event.preventDefault();
-       // prevent default refresh on anchors
-       });
-    }
+const logo = document.querySelector('.logo');
+logo.addEventListener('click', (event) => {
+    event.preventDefault();
+})
